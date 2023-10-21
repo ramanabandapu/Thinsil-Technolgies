aa7160b7850f44df28b6cea9eab14b164dc41758
Ramana Bandapu <bandapu.ramana@gmail.com>
2023-10-21 22:57:19 +0530

    backend setup completed

---
+++ e-commerce-website/src/Pages/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send a POST request to your backend API for user login
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ e-commerce-website/src/Pages/Signup.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+function Signup() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Send a POST request to your backend API for user registration
+    // You can use a library like axios to make API requests.
+  };
+
+  return (
+    <div>
+      <h2>Register Here</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleInputChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
